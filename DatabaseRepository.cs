bf25fd8b3d7d4a8d040c6f313d32d25aa8f4e1f4
krister <krister@trangius.se>
2024-12-11 15:24:19 +0100

    start using connection pooling instead

--- DatabaseRepository.cs
+++ DatabaseRepository.cs
@@ -6,33 +6,37 @@ namespace MySchoolProgram;
 
 class DatabaseRepository
 {
+    string connectionstring = File.ReadAllText("connectionstring.txt");
+    
+    public IDbConnection Connect()
+    {
+        return new SqlConnection(connectionstring);
+    }
+
     public IEnumerable<Student> GetStudents()
     {
-        string connectionstring = File.ReadAllText("connectionstring.txt");
-        using IDbConnection connection = new SqlConnection(connectionstring);
+        // skapa en enkel textsträng som innehåller connection string
+        using IDbConnection connection = Connect();
         return connection.Query<Student>("SELECT * FROM Students");
     }
 
     public void InsertStudent(string name, string email, DateTime dateOfBirth)
     {
         // Spara i datbasen
-        string connectionstring = File.ReadAllText("connectionstring.txt");
-        using IDbConnection connection = new SqlConnection(connectionstring);
         string query = $"INSERT INTO Students (Name, Email, DateOfBirth) VALUES ('{name}', '{email}', '{dateOfBirth}')";
         // Console.WriteLine(query);
         // anslut till databasen med hjälp av connection string
+        using IDbConnection connection = Connect();
         connection.Execute(query);
     }
     
     public void UpdateStudent(int id, string name, string email, DateTime dateOfBirth)
     {
         // Spara i datbasen
-        string connectionstring = File.ReadAllText("connectionstring.txt");
-        using IDbConnection connection = new SqlConnection(connectionstring);
         string query = $" UPDATE Students SET Name = '{name}', Email = '{email}', DateOfBirth = '{dateOfBirth}' WHERE Id = {id}"; 
-        
         // Console.WriteLine(query);
         // anslut till databasen med hjälp av connection string
+        using IDbConnection connection = Connect();
         connection.Execute(query);
     }
 }
